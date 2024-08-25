08885b91dba63c076973a54ee09ec87ba6b0ad34
munna710 <munna7777777.1.0@gmail.com>
2024-08-25 16:41:42 +0530

    chore: Update customer and order views and templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('products/', views.products, name='products'),
     path('customers/<str:pk>/', views.customers, name='customers'),
+    path('create_order/', views.createOrder, name='create_order'),
+    path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
+    path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from .models import *
+from .forms import OrderForm
 def dashboard(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -25,3 +26,38 @@ def customers(request,pk):
     
     context = {'customer':customer, 'orders':orders, 'order_count':order_count}
     return render(request, 'accounts/customer.html', context)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    
+    context = {'item':order}
+    return render(request, 'accounts/delete_item.html', context)
