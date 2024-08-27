9b0fe7eeaf96627b3a02baaae77a8c588c79d0cc
munna710 <munna7777777.1.0@gmail.com>
2024-08-27 22:06:02 +0530

    chore: Add reset password

--- crm/settings.py
+++ crm/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +136,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'munnazaishy@gmail.com'
+EMAIL_HOST_PASSWORD = 'jzzcnbalqtywdpnb'
+EMAIL_USE_SSL = False
